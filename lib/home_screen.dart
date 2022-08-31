ad128236aa3a2bc8da8b4c5e73409cc1b8bdd7d4
Rittik <rittik2804@gmail.com>
2022-08-31 15:15:42 +0530

    third commit

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
 import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import 'package:webview_flutter/webview_flutter.dart';
 import 'no_internet.dart';
@@ -80,133 +81,169 @@ class _pageUiState extends State<pageUi> {
           }
         },
         child: Scaffold(
-          drawer: Drawer(
-            // Add a ListView to the drawer. This ensures the user can scroll
-            // through the options in the drawer if there isn't enough vertical
-            // space to fit everything.
-            child: ListView(
-              // Important: Remove any padding from the ListView.
-              padding: EdgeInsets.zero,
-              children: [
-                DrawerHeader(
-                  child: Container(
-                    decoration: BoxDecoration(
-                        color: Color.fromRGBO(76, 68, 108, 1),
-                        image: DecorationImage(
-                            image: AssetImage("assets/logo.png")),
-                        borderRadius: BorderRadius.circular(5)),
-                    // child: Image(
+            drawer: Drawer(
+              // Add a ListView to the drawer. This ensures the user can scroll
+              // through the options in the drawer if there isn't enough vertical
+              // space to fit everything.
+              child: ListView(
+                // Important: Remove any padding from the ListView.
+                padding: EdgeInsets.zero,
+                children: [
+                  DrawerHeader(
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: Color.fromRGBO(76, 68, 108, 1),
+                          image: DecorationImage(
+                              image: AssetImage("assets/logo.png")),
+                          borderRadius: BorderRadius.circular(5)),
+                      // child: Image(
 
-                    //     image: AssetImage("assets/logo.png")),
+                      //     image: AssetImage("assets/logo.png")),
+                    ),
                   ),
-                ),
-                ListTile(
-                  title: const Text(
-                    'Home',
-                    style: TextStyle(fontSize: 21),
+                  ListTile(
+                    title: const Text(
+                      'Home',
+                      style: TextStyle(fontSize: 21),
+                    ),
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: ((context) => pageUi())));
+                    },
                   ),
-                  onTap: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: ((context) => pageUi())));
-                  },
-                ),
-                Divider(
-                  color: Colors.black,
-                ),
-                ListTile(
-                    title: const Text(
-                      'Become A Seller',
-                      style: TextStyle(fontSize: 21),
-                    ),
-                    onTap: () {
-                      final url = controller.currentUrl();
-                      controller
-                          .loadUrl("https://lakhthakurdwara.com/shops/create");
-                    }),
-                Divider(
-                  color: Colors.black,
-                ),
-                ListTile(
-                    title: const Text(
-                      "User Login",
-                      style: TextStyle(fontSize: 21),
-                    ),
-                    onTap: () {
-                      final url = controller.currentUrl();
-                      controller
-                          .loadUrl("https://lakhthakurdwara.com/users/login");
-                    }),
-                Divider(
-                  color: Colors.black,
-                ),
-                ListTile(
-                  title: const Text(
-                    'All Categories',
-                    style: TextStyle(fontSize: 21),
+                  Divider(
+                    color: Colors.black,
                   ),
-                  onTap: () {
-                    final url = controller.currentUrl();
-                    controller.loadUrl("https://lakhthakurdwara.com/search");
-                  },
-                ),
-                Divider(
-                  color: Colors.black,
-                ),
-                ListTile(
+                  ListTile(
+                      title: const Text(
+                        'Become A Seller',
+                        style: TextStyle(fontSize: 21),
+                      ),
+                      onTap: () {
+                        final url = controller.currentUrl();
+                        controller.loadUrl(
+                            "https://lakhthakurdwara.com/shops/create");
+                      }),
+                  Divider(
+                    color: Colors.black,
+                  ),
+                  ListTile(
+                      title: const Text(
+                        "User Login",
+                        style: TextStyle(fontSize: 21),
+                      ),
+                      onTap: () {
+                        final url = controller.currentUrl();
+                        controller
+                            .loadUrl("https://lakhthakurdwara.com/users/login");
+                      }),
+                  Divider(
+                    color: Colors.black,
+                  ),
+                  ListTile(
                     title: const Text(
-                      "Register As Customer",
+                      'All Categories',
                       style: TextStyle(fontSize: 21),
                     ),
                     onTap: () {
                       final url = controller.currentUrl();
-                      controller.loadUrl(
-                          "https://lakhthakurdwara.com/users/registration");
-                    }),
-                Divider(
-                  color: Colors.black,
-                ),
-                ListTile(
-                    title: const Text(
-                      "Cart",
-                      style: TextStyle(fontSize: 21),
-                    ),
-                    onTap: () {
-                      final url = controller.currentUrl();
-                      controller.loadUrl("https://lakhthakurdwara.com/cart");
-                    }),
-                Divider(
-                  color: Colors.black,
-                ),
-              ],
+                      controller.loadUrl("https://lakhthakurdwara.com/search");
+                    },
+                  ),
+                  Divider(
+                    color: Colors.black,
+                  ),
+                  ListTile(
+                      title: const Text(
+                        "Register As Customer",
+                        style: TextStyle(fontSize: 21),
+                      ),
+                      onTap: () {
+                        final url = controller.currentUrl();
+                        controller.loadUrl(
+                            "https://lakhthakurdwara.com/users/registration");
+                      }),
+                  Divider(
+                    color: Colors.black,
+                  ),
+                  ListTile(
+                      title: const Text(
+                        "Cart",
+                        style: TextStyle(fontSize: 21),
+                      ),
+                      onTap: () {
+                        final url = controller.currentUrl();
+                        controller.loadUrl("https://lakhthakurdwara.com/cart");
+                      }),
+                  Divider(
+                    color: Colors.black,
+                  ),
+                ],
+              ),
             ),
-          ),
-          appBar: AppBar(
-            backgroundColor: Color.fromRGBO(76, 68, 108, 1),
-            title: GestureDetector(
-              onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: ((context) => pageUi())));
-              },
-              child: Container(
-                height: size.height / 15,
-                child: Image(
-                  image: AssetImage("assets/appbar_logo.png"),
+            appBar: AppBar(
+              backgroundColor: Color.fromRGBO(76, 68, 108, 1),
+              title: GestureDetector(
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: ((context) => pageUi())));
+                },
+                child: Container(
+                  height: size.height / 15,
+                  child: Image(
+                    image: AssetImage("assets/appbar_logo.png"),
+                  ),
                 ),
               ),
             ),
-          ),
-          resizeToAvoidBottomInset: false,
-          body: WebView(
-            onWebViewCreated: ((controller) {
-              this.controller = controller;
-            }),
-            onProgress: (url) async {
-              controller.evaluateJavascript(
-                  "document.getElementsByTagName('header')[0].style.display='none'");
-            },
-            initialUrl: ('https://lakhthakurdwara.com/'),
-            javascriptMode: JavascriptMode.unrestricted,
-          ),
-        ));
+            resizeToAvoidBottomInset: false,
+            body: Container(
+              child: WebView(
+                initialUrl: ('https://lakhthakurdwara.com/'),
+                javascriptMode: JavascriptMode.unrestricted,
+                onWebViewCreated: ((WebViewController controller) {
+                  this.controller = controller;
+                }),
+                onProgress: (url) async {
+                  controller.evaluateJavascript(
+                      "document.getElementsByTagName('header')[0].style.display='none'");
+                },
+                navigationDelegate: (NavigationRequest request) {
+                  if (request.url.startsWith("https://lakhthakurdwara.com/")) {
+                    return NavigationDecision.navigate;
+                  } else {
+                    _launchURL(request.url);
+                    return NavigationDecision.prevent;
+                  }
+                },
+              ),
+            )));
   }
+
+  _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  
 }
+
+
+
+
+
+          
+          // body: WebView(
+          //   onWebViewCreated: ((controller) {
+          //     this.controller = controller;
+          //   }),
+            // onProgress: (url) async {
+            //   controller.evaluateJavascript(
+            //       "document.getElementsByTagName('header')[0].style.display='none'");
+            // },
+          //   initialUrl: ('https://lakhthakurdwara.com/'),
+          //   javascriptMode: JavascriptMode.unrestricted,
+          // ),
